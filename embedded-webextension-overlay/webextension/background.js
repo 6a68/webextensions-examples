730c56a5c43424636fdecad0b32f49bb18c12254
Jared Hirsch <ohai@6a68.net>
2017-10-05 16:27:43 -0700

    Add storage.local invocation to overlay webextension example

--- embedded-webextension-overlay/webextension/background.js
+++ embedded-webextension-overlay/webextension/background.js
@@ -1,3 +1,13 @@
 console.log("Embedded WebExtension", window.location.href);
 
 browser.runtime.sendMessage("embedded_webext -> overlay addon container");
+
+let onError = (err) => { console.error(err) };
+
+console.log('writing foo = 42 to local storage...');
+browser.storage.local.set({foo: 42}).then(() => {
+  console.log('reading from local storage...');
+  browser.storage.local.get('foo').then((result) => {
+    console.log('the value of foo is ', result.foo);
+  }, onError);
+}, onError);
